bff79af89abd40ad9d5b493e760f40120bda7e52
At0m234 <v.o.ilin@yandex.ru>
2023-04-25 20:18:51 +0300

    added 404 and 500 routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import notFoundPage from '../views/notFoundPage.vue'
+import serverError from '../views/serverError.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/500',
+      name: 'serverError',
+      component: serverError,
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFoundPage',
+      component: notFoundPage,
     },
   ],
 })
