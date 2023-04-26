03f78d25f3559792a5cf363162ec00077757c1ee
At0m234 <v.o.ilin@yandex.ru>
2023-04-26 23:19:11 +0300

    added const organization info

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,17 @@
 export const HEAD_CONST = {
-    TIME_START : '08:00',
-    TIME_FINISH : '05:00',
-    CITY: 'Тула',
-    DELIVERY_TIME: 'от 45-50 минут',
-    PHONE: '+7(499)999 99 99'
+  TIME_START: '08:00',
+  TIME_FINISH: '05:00',
+  CITY: 'Тула',
+  DELIVERY_TIME: 'от 45-50 минут',
+  PHONE: '+7(499)999 99 99',
+}
+
+export const ORGANIZATION_INFO = {
+  NAME: 'ООО «СОК»',
+  ADRESS:
+    '111672, г. Москва, ул. Новокосинская, д. 20, корп. 2., этаж 1, помещение Iб, комната 2, офис 2б',
+  OGRN: '1147746474226',
+  INN: ' 7720812834',
+  KPP: '772001001',
+  PHONE: ' +7 (495) 287-65-00',
 }
