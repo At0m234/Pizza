6228aa2159e4c7a36cad1dae2d5c8d9ea7d73613
At0m234 <v.o.ilin@yandex.ru>
2023-05-07 20:50:04 +0300

    remove unused imports

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import CustomCarousel from '@/components/CustomCarousel.vue'
 import MenuIcons from '@/components/MenuIcons.vue'
-import ProductCard from "@/components/ProductCard.vue";
 import ProductCategory from "@/components/ProductCategory.vue";
 
 </script>
