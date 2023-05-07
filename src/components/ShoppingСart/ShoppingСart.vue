76c9af5525ac27236cd0f9f0921f1bf8e9a9e4ce
At0m234 <v.o.ilin@yandex.ru>
2023-05-07 20:25:35 +0300

    added shoppingCart test layuot

---
+++ src/components/ShoppingСart/ShoppingСart.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <section class="shopping-cart">
+    
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+@media (max-width: 900px) {
+
+}
+</style>
