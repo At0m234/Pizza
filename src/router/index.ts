955224cfbfa47cfc13452074cd2c07b7123fe089
At0m234 <v.o.ilin@yandex.ru>
2023-05-28 23:31:52 +0300

    added order-registration route

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import AboutCompanyView from '../views/AboutCompanyView.vue'
 import DeliveryView from '../views/DeliveryView.vue'
 import DiscountView from '../views/DiscountView.vue'
 import ShoppingCartView from '../views/ShoppingCartView.vue'
+import OrderRegistrationView from '../views/OrderRegistrationView.vue'
 import DiscountCard from '../components/UI/DiscountCard.vue'
 
 const router = createRouter({
@@ -32,29 +33,41 @@ const router = createRouter({
       component: ShoppingCartView,
     },
     {
+      path: '/order-registration',
+      name: 'OrderRegistration',
+      component: OrderRegistrationView,
+    },
+    {
       path: '/discounts',
       name: 'discount',
       component: DiscountView,
-      children: [
-        {
-          path: ':id',
-          name: 'DiscountCard',
-          component: DiscountCard,
-          props: route => ({
-            id: route.params.id,
-            title: route.params.title,
-            text: route.params.text,
-            promo: route.params.promo,
-            imageUrl: route.params.imageUrl,
-          }),
-        },
-      ],
+      // children: [
+      //   {
+      //     path: '/:id',
+      //     name: 'DiscountCard',
+      //     component: DiscountCard,
+      //     props: route => ({
+      //       id: route.params.id,
+      //       title: route.params.title,
+      //       text: route.params.text,
+      //       promo: route.params.promo,
+      //       imageUrl: route.params.imageUrl,
+      //     }),
+      //   },
+      // ],
     },
-    // {
-    //   path: '/discounts/:id',
-    //   name: 'DiscountCard',
-    //   component: DiscountCard,
-    // },
+    {
+      path: '/discounts/:id',
+      name: 'DiscountCard',
+      component: DiscountCard,
+      props: route => ({
+        id: route.params.id,
+        title: route.params.title,
+        text: route.params.text,
+        promo: route.params.promo,
+        imageUrl: route.params.imageUrl,
+      }),
+    },
     {
       path: '/500',
       name: 'serverError',
