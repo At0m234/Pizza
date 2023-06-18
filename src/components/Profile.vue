2296e8908c95fcf39235e176b40fc611b71c2073
At0m234 <v.o.ilin@yandex.ru>
2023-06-18 22:24:13 +0300

    updated Profile component right rendering on navLinks click

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-// import CustomButton from '@/components/UI/CustomButton.vue';
-
-const profileNavigation = [
-  { index: 0, active: true, title: 'Мои данные', component: 'ProfileMyData' },
-  { index: 1, active: false, title: 'Адреса', component: 'ProfileAddresses' },
-  { index: 2, active: false, title: 'История заказов', component: 'ProfileOrderHistory' },
-];
-
-const activeComponent = ref(profileNavigation[0].component);
+import { ref, type Component } from 'vue';
+import CustomButton from '@/components/UI/CustomButton.vue';
+import ProfileMyData from '../components/ProfileMyData.vue';
+import ProfileAddresses from '../components/ProfileAddresses.vue';
+import ProfileOrderHistory from '../components/ProfileOrderHistory.vue';
 
 interface Button {
-  title: string
   index: number
   active: boolean
-  component: 'ProfileMyData' | 'ProfileAddresses' | 'ProfileOrderHistory'
+  title: string
+  component: Component;
 }
 
-function navigateTo(buttons: Button[], index: number, component: string) {
+const profileNavigation: Button[] = [
+  { index: 0, active: true, title: 'Мои данные', component: ProfileMyData },
+  { index: 1, active: false, title: 'Адреса', component: ProfileAddresses },
+  { index: 2, active: false, title: 'История заказов', component: ProfileOrderHistory },
+];
+
+let activeComponent = ref<Component>(profileNavigation[0].component);
+
+function navigateTo(buttons: Button[], index: number, component: Component) {
   buttons.forEach((button, i) => {
     button.active = i === index // Устанавливаем активное состояние только для выбранной кнопки
-    activeComponent.value = component;
+    activeComponent.value = component as Component;
   })
 }
 </script>
@@ -28,13 +31,13 @@ function navigateTo(buttons: Button[], index: number, component: string) {
 <template>
   <section class="profile">
     <nav class="profile__navigation">
-      <!-- <CustomButton v-for="(item, index) in profileNavigation" :key="index"
+      <CustomButton v-for="(item, index) in profileNavigation" :key="index"
         @click="navigateTo(profileNavigation, index, item.component)" :active="item.active" :text="item.title"
-        :customStyles="{ borderRadius: '10px', marginRight: '50px', padding: '5px 10px' }">
-      </CustomButton> -->
+        :customStyles="{ borderRadius: '10px', padding: '5px 10px', marginBottom: '20px' }">
+      </CustomButton>
     </nav>
     <div class="profile__content">
-      <component :is="activeComponent" />
+      <component :is="activeComponent" v-if="activeComponent" />
     </div>
   </section>
 </template>
@@ -47,24 +50,25 @@ function navigateTo(buttons: Button[], index: number, component: string) {
   &__navigation {
     display: flex;
     align-items: center;
-    margin: 40px auto 20px 0;
+    justify-content: space-between;
+    max-width: 550px;
+    margin: 40px 0 0 0;
   }
 
   &__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     max-width: 1040px;
     border: 1px solid var(--border-order);
     border-radius: 10px;
-    padding: 50px 200px;
   }
 }
 
-.nav-link {
-  // Добавьте стили для ссылок в навигации
+@media (max-width: 520px) {
+  .profile__navigation {
+    flex-direction: column;
+  }
 }
-
-.nav-link.active {
-  // Добавьте стили для активной ссылки
-}
-
-@media (max-width: 744px) {}
 </style>
