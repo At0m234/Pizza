003a730680c3938aeeea8ca395efd583c95fc89e
At0m234 <v.o.ilin@yandex.ru>
2023-06-20 21:45:56 +0300

    added case where no orders. fixed styles.

--- src/components/ProfileOrderHistory.vue
+++ src/components/ProfileOrderHistory.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 import Order from '@/components/UI/Order.vue'
+import router from '../router/'
+
+const hasOrders = true
 
 const order = [
   {
@@ -98,24 +101,36 @@ const order2 = [
   },
 ]
 
+function goToMenu() {
+  router.push('/main')
+}
 </script>
 
 <template>
-  <div class="order-list">
-    <Order date="10.05.2023" number="№ 1234" :orderList="order" :delivery-summ="350" :total-summ="1000" :history="true" />
-    <Order date="10.05.2023" number="№ 1234" :orderList="order1" :delivery-summ="350" :total-summ="1000"
-      :history="true" />
-    <Order date="10.05.2023" number="№ 1234" :orderList="order2" :delivery-summ="350" :total-summ="1000"
-      :history="true" />
-  </div>
+  <template v-if="hasOrders">
+    <div class="order-list scroll">
+      <Order date="10.05.2023" number="№ 1111" :orderList="order" :delivery-summ="350" :total-summ="1000"
+        :history="true" />
+      <Order date="10.05.2023" number="№ 2222" :orderList="order1" :delivery-summ="350" :total-summ="1000"
+        :history="true" />
+      <Order date="10.05.2023" number="№ 3333" :orderList="order2" :delivery-summ="350" :total-summ="1000"
+        :history="true" />
+    </div>
+  </template>
+  <template v-else>
+    <div class="no-orders">
+      <h2 class="no-orders__title">У вас пока нет ни одного заказа. Перейти в <span @click="goToMenu">МЕНЮ</span></h2>
+    </div>
+  </template>
 </template>
 
 <style lang="scss" scoped>
 .order-list {
   display: flex;
   flex-direction: column;
+  align-items: center;
   width: 100%;
-  max-height: 650px;
+  max-height: 550px;
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
@@ -128,6 +143,32 @@ const order2 = [
 
   &::-webkit-scrollbar-track {
     background-color: transparent;
+  }
+}
+
+.no-orders {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 400px;
+
+  &__title {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    color: var(--color-text-black);
+
+    & span {
+      cursor: pointer;
+      color: var(--color-warning);
+
+      &:hover {
+        transform: scale(1.25);
+      }
+    }
   }
 }
 </style>
